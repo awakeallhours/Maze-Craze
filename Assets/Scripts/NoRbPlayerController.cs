7cf2d89b21ed147fb1a46cdfdda33dcb29f21afe
awakeallhours <133714102+awakeallhours@users.noreply.github.com>
2025-02-05 21:52:41 +0000

    Player controller working
    Added jump crouch and sprint functionality. with changes to movement speed.  Also corrected gravity to a minus number as is was positive and making the player fly....

--- Assets/Scripts/NoRbPlayerController.cs
+++ Assets/Scripts/NoRbPlayerController.cs
@@ -1,14 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 
 
 public class NoRbPlayerController : MonoBehaviour
 {
-    //[SerializeField, Tooltip("Player prefab")] GameObject player;
-    //[SerializeField, Tooltip("Player height")] float playerHeight;
-    [SerializeField] float currentSpeed; 
     
+    [SerializeField, Tooltip("Player height")] float playerHeight;
+    [SerializeField, Tooltip("Crouch height, default is half of player height")] float crouchHeight;
+    [SerializeField] float currentSpeed;
+   
     public float baseSpeed = 5f;
     public float gravity = -9.81f;
     public float jumpHeight = 1f;
@@ -16,52 +19,92 @@ public class NoRbPlayerController : MonoBehaviour
     private CharacterController controller;
     private Vector3 velocity;
     private bool isGrounded;
+    private bool isCrouching = false;
+    public bool isSprinting = false;
 
     void Start()
     {
+        // assigns the controller to the character controller, also uses the controller to set the height of the player for the crouch mechanic
         controller = GetComponent<CharacterController>();
-        //playerHeight = player.transform.position.y;
-        //float crouchHeight = playerHeight / 2;
+        playerHeight = controller.height;
+        crouchHeight = playerHeight / 2;
     }
 
     void Update()
     {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        //enures player is grounded
         isGrounded = controller.isGrounded;
         if ((isGrounded && velocity.y < 0))
         {
-            velocity.y = 0f;
+            velocity.y = -2f;
+            
         }
 
+
+        // movement variables
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
-
         Vector3 move = transform.right * x + transform.forward * z;
-       
+        
+        //Moves the player
+        controller.Move(move * currentSpeed * Time.deltaTime);
         currentSpeed = baseSpeed;
-        if (Input.GetKey(KeyCode.LeftShift))
+
+        //sprint
+        if (Input.GetKey(KeyCode.LeftShift) && !isSprinting)
+        {
+            isSprinting = true;
+           
+
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift) && isSprinting)
+        {
+            isSprinting = false;
+        }
+        if (isSprinting)
         {
             currentSpeed *= 2;
         }
 
-        controller.Move(move * currentSpeed * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        
+        
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
+            
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            
         }
 
 
-        if (Input.GetKey(KeyCode.LeftControl))
+        //crouch
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouching)
         {
-            //todo crouch logic
+            controller.height = crouchHeight;
+            isCrouching = true;
+            
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl) && isCrouching)
+        {
+            controller.height = playerHeight;
+            isCrouching = false;
+
+        }
+
+        if (isCrouching)
+        {
+            currentSpeed /= 2;
         }
         
-        
-        //why is the player taking off?????????
-        //its something to do with this velocity line
-        
-        velocity.y -= gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime); 
+
+        //gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
 }
