4179813bc02e91849629ad9492720cbaae82b846
awakeallhours <133714102+awakeallhours@users.noreply.github.com>
2025-02-06 20:43:24 +0000

    Modified player controller
    Added more booleans.
    sprinting, moving, crouching, sneaking, jumping.
    
    Fixed falling through floor on crouching

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     [SerializeField, Tooltip("Mouse sensitivity")] float mouseSensitivity = 100f;
+   
     public Transform playerPrefab;
+    private NoRbPlayerController controller;
+    
 
     float xRotation = 0f;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        controller = playerPrefab.GetComponent<NoRbPlayerController>();
+       
     }
 
     
@@ -20,7 +25,17 @@ public class MouseLook : MonoBehaviour
         xRotation -= MouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
+       
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerPrefab.Rotate(Vector3.up * mouseX);
+
+        if (controller.isCrouching)
+        {
+            transform.position = new Vector3(transform.position.x, controller.crouchHeight, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, controller.transform.position.y, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/NoRbPlayerController.cs
+++ Assets/Scripts/NoRbPlayerController.cs
@@ -3,31 +3,39 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
-
-
 public class NoRbPlayerController : MonoBehaviour
 {
+    //playerheight is set to controller height
+    public float playerHeight;
+    private float gravity = -9.81f;
+
+    //player position logs
+    private Vector3 playerPos;
+    private Vector3 lastPlayerPos;
     
-    [SerializeField, Tooltip("Player height")] float playerHeight;
-    [SerializeField, Tooltip("Crouch height, default is half of player height")] float crouchHeight;
-    [SerializeField] float currentSpeed;
-   
-    public float baseSpeed = 5f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 1f;
+    [SerializeField, Tooltip("Crouch height divisor, default is 2. Which sets crouch to half of the player height")] public float crouchHeight = 2f;
+    [SerializeField, Tooltip("Player jump height")] float jumpHeight = 1f;
+    [SerializeField, Tooltip("The base movement speed for the player")] float baseSpeed = 5f;
+    [SerializeField, Tooltip("Allows us to view the current speed, not neccessary but useful. Value should NOT be edited manually.")] float currentSpeed;
+    
+    //bools
+    public bool isGrounded;
+    public bool isJumping = false;
+    public bool isCrouching = false;
+    public bool isSprinting = false;
+    public bool isMoving = false;
+    public bool isSneaking = false;
 
     private CharacterController controller;
     private Vector3 velocity;
-    private bool isGrounded;
-    private bool isCrouching = false;
-    public bool isSprinting = false;
 
     void Start()
     {
-        // assigns the controller to the character controller, also uses the controller to set the height of the player for the crouch mechanic
         controller = GetComponent<CharacterController>();
+        
         playerHeight = controller.height;
-        crouchHeight = playerHeight / 2;
+        crouchHeight = playerHeight / crouchHeight;
+
     }
 
     void Update()
@@ -37,74 +45,83 @@ public class NoRbPlayerController : MonoBehaviour
 
     void PlayerMovement()
     {
-        //enures player is grounded
+        //logging player position at start of loop
+        lastPlayerPos = transform.position;
+
         isGrounded = controller.isGrounded;
-        if ((isGrounded && velocity.y < 0))
+        if (isGrounded && velocity.y < 0)
         {
-            velocity.y = -2f;
-            
+            //because velocity.y is contantly decreasing due to gravity, this set it to -1, if set to 0 isGrounded no longer works properly
+            velocity.y = -1f;
+            isJumping = false;
         }
 
-
-        // movement variables
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         Vector3 move = transform.right * x + transform.forward * z;
-        
-        //Moves the player
+
         controller.Move(move * currentSpeed * Time.deltaTime);
         currentSpeed = baseSpeed;
 
-        //sprint
-        if (Input.GetKey(KeyCode.LeftShift) && !isSprinting)
-        {
-            isSprinting = true;
-           
-
-        }
-        else if (Input.GetKeyUp(KeyCode.LeftShift) && isSprinting)
-        {
-            isSprinting = false;
-        }
-        if (isSprinting)
+        if (Input.GetKey(KeyCode.LeftShift))
         {
             currentSpeed *= 2;
         }
+        else
+        {
+            isSprinting = false;
+        }
 
-        
-        
-        //jump
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
-            
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            isJumping = true;
             
         }
 
-
-        //crouch
         if (Input.GetKeyDown(KeyCode.LeftControl) && !isCrouching)
         {
             controller.height = crouchHeight;
+            controller.center = new Vector3(controller.center.x, controller.center.y - (playerHeight - crouchHeight) / 2, controller.center.z);
             isCrouching = true;
-            
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl) && isCrouching)
         {
             controller.height = playerHeight;
+            controller.center = new Vector3(controller.center.x, controller.center.y + (playerHeight - crouchHeight) / 2, controller.center.z);
             isCrouching = false;
-
         }
 
         if (isCrouching)
         {
             currentSpeed /= 2;
         }
-        
 
-        //gravity
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        if (!float.IsNaN(velocity.y) && !float.IsInfinity(velocity.y))
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+        //isMoving checks 
+
+        //logging player position at end of loop
+        playerPos = transform.position;
+
+        //setting isMoving to true if position don't match
+        isMoving = playerPos != lastPlayerPos;
+
+        //sneaking check for isMoving
+        isSneaking = isCrouching && isMoving;
+
+        //sprinting check for isMoving
+        if (currentSpeed > baseSpeed && isMoving)
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
     }
-
 }
