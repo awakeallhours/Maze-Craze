11b292618fd1a5c6d36f89d5b5f6d5125813cf27
awakeallhours <133714102+awakeallhours@users.noreply.github.com>
2025-02-16 22:07:10 +0000

    Revert "Squashed commit of the following:"
    This reverts commit 7a80a86e596d4ea8d7ffa381199f075d9786a5db.

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -1,23 +1,14 @@
-using FMOD;
-using FMODUnity;
 using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
     Light torch;
-    StudioEventEmitter playerTorchToggleEventEmitter;
-    float isOnFMODParameter = 0f;
+
     public bool isOn = false;
-
-    float debugFMODParameter;
-
     void Start()
     {
         torch = GetComponent<Light>();  
         torch.enabled = false;
-
-        //FMOD
-        playerTorchToggleEventEmitter = gameObject.GetComponent<StudioEventEmitter>();
     }
 
     void Update()
@@ -30,18 +21,8 @@ public class Torch : MonoBehaviour
 
     void ToggleTorch()
     {
+        
         isOn = !isOn;
         torch.enabled = isOn;
-
-        isOnFMODParameter = isOn ? 1f : 0f;
-
-        UnityEngine.Debug.Log("isOn: - " + isOn); 
-        UnityEngine.Debug.Log("pre FMOD - " + isOnFMODParameter);
-
-        playerTorchToggleEventEmitter.EventInstance.setParameterByName("isOn", isOnFMODParameter, false);
-        playerTorchToggleEventEmitter.Play();
-        
-        playerTorchToggleEventEmitter.EventInstance.getParameterByName("isOn", out debugFMODParameter);
-        UnityEngine.Debug.Log("post FMOD - " + debugFMODParameter);
     }
 }
