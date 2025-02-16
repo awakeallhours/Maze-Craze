43fff667e05fc174d3ed6251f126e3bb331faf50
awakeallhours <133714102+awakeallhours@users.noreply.github.com>
2025-02-16 20:39:29 +0000

    Hud elements update, player tweaks
    Hud elements now update
    player has health, stamina and battery attributes. Health is not yet implemented. Some revisions to player controller

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -2,27 +2,63 @@ using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
+    [SerializeField, Tooltip("Torch intesity default value is 3")] float torchIntensity; 
+    private PlayerAttributes attributes;
     Light torch;
 
     public bool isOn = false;
     void Start()
     {
-        torch = GetComponent<Light>();  
+        torch = GetComponent<Light>();
         torch.enabled = false;
+        torch.intensity = torchIntensity;
+
+        attributes = GetComponentInParent<PlayerAttributes>();
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        if (Input.GetKeyDown(KeyCode.F) || !attributes.torchAllowed)
         {
             ToggleTorch();
+        }
+       
+        if(!attributes.torchAllowed)
+        {
+            isOn = false;
+            torch.enabled = false;
         }
     }
 
     void ToggleTorch()
     {
+        if (attributes.torchAllowed)
+        {
+            isOn = !isOn;
+            torch.enabled = isOn;
+        }
+
+        if(isOn)
+        {
+            TorchFlicker();
+            Debug.Log("Flickering");
+        }
+    }
+
+    void TorchFlicker()
+    {
+        float flickerAmount = 0;
+
+        if (attributes.currentBattery >= 40)
+        {
+            flickerAmount = Random.Range(-10f, 10f) * Time.deltaTime;
         
-        isOn = !isOn;
-        torch.enabled = isOn;
+        }
+        else if (attributes.currentBattery < 40)
+        {
+            flickerAmount = Random.Range(torchIntensity / 2, 1f);
+        }
+
+        torch.intensity = torchIntensity + flickerAmount;
     }
 }
