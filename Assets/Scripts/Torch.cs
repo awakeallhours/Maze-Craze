7a80a86e596d4ea8d7ffa381199f075d9786a5db
DJMullaney1 <davidmullaneyofficial@gmail.com>
2025-02-16 20:54:08 +0000

    Squashed commit of the following:
    commit 514481b78d3ca9fee4e6fd2f8be24f3b50ff1818
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Sun Feb 16 20:26:40 2025 +0000
    
        Fixed footstep surface detection
    
        Footstep surface detection may change in the future but at least it works as intended now
    
    commit 2e8f1f61b37989e6093260ec50548e0764b99880
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Sun Feb 16 20:06:53 2025 +0000
    
        Footsteps audio improvements (multi-instruments, samples, FMOD structure) FMOD Parameters not working still, needs investigating further
    
        I need a break R.I.P
    
    commit effa92ccab07000be6a8edc4c9a6d4d5b95536dc
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Sat Feb 15 17:18:40 2025 +0000
    
        .gitignore refresh
    
    commit 6121d8d469f38523df4f97e696c1053463a88113
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Sat Feb 15 17:15:50 2025 +0000
    
        Footsteps terrainType work in progress & additions to .gitignore
    
        Need to revisit with fresh eyes, currently footstep ground raycasting is not detecting layers as expected. Added additional exclusions to .gitignore
    
    commit 555dc0779b3c88692c6f32bf1ee96df8f86583b3
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Sat Feb 15 14:14:32 2025 +0000
    
        .gitignore update (exclude standalone FMOD Studio cache)
    
        Expanded the scope for the FMOD for Unity cache folder to include everything inside. Added additional exclusions for FMOD Studio (Standalone) project files that are not needed for source control
    
    commit af08d3b17435e49107aaae55d58be48d62811a09
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Fri Feb 14 18:29:14 2025 +0000
    
        Fixed footstepDelayTime not scaling with baseSpeed
    
        Minor footstep audio improvements
    
    commit 6d9a7bb860a8e556804cc3cf51abd2f78c29f642
    Author: DJMullaney1 <davidmullaneyofficial@gmail.com>
    Date:   Thu Feb 13 18:20:28 2025 +0000
    
        Squashed commit of the following:
    
        commit f7abdca9ad1e5e0de2a284c1e911656502e8cca1
        Author: awakeallhours <133714102+awakeallhours@users.noreply.github.com>
        Date:   Wed Feb 12 23:54:57 2025 +0000
    
            Tidying up.... Except for Davids mess
    
        commit eac7c0d007ce104468236044422e119ba95d0174
        Author: awakeallhours <133714102+awakeallhours@users.noreply.github.com>
        Date:   Tue Feb 11 21:40:06 2025 +0000
    
            Changed torch position
    
        commit e88c5dda6faa778954cfe95f04e0c20875447908
        Author: awakeallhours <133714102+awakeallhours@users.noreply.github.com>
        Date:   Tue Feb 11 21:28:42 2025 +0000
    
            Fixed door flying off hinges
    
            Used longer screws to stop David kicking the door off its hinges

--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -1,14 +1,23 @@
+using FMOD;
+using FMODUnity;
 using UnityEngine;
 
 public class Torch : MonoBehaviour
 {
     Light torch;
-
+    StudioEventEmitter playerTorchToggleEventEmitter;
+    float isOnFMODParameter = 0f;
     public bool isOn = false;
+
+    float debugFMODParameter;
+
     void Start()
     {
         torch = GetComponent<Light>();  
         torch.enabled = false;
+
+        //FMOD
+        playerTorchToggleEventEmitter = gameObject.GetComponent<StudioEventEmitter>();
     }
 
     void Update()
@@ -21,8 +30,18 @@ public class Torch : MonoBehaviour
 
     void ToggleTorch()
     {
-        
         isOn = !isOn;
         torch.enabled = isOn;
+
+        isOnFMODParameter = isOn ? 1f : 0f;
+
+        UnityEngine.Debug.Log("isOn: - " + isOn); 
+        UnityEngine.Debug.Log("pre FMOD - " + isOnFMODParameter);
+
+        playerTorchToggleEventEmitter.EventInstance.setParameterByName("isOn", isOnFMODParameter, false);
+        playerTorchToggleEventEmitter.Play();
+        
+        playerTorchToggleEventEmitter.EventInstance.getParameterByName("isOn", out debugFMODParameter);
+        UnityEngine.Debug.Log("post FMOD - " + debugFMODParameter);
     }
 }
